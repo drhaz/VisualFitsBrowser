46a698c711ab72c34394a36884487146185e5b0b
Daniel Harbeck <drharbeck@gmail.com>
2016-03-03 11:02:10 -0700

    Initial load

---
+++ VisualFitsBrowser/src/main/java/org/wiyn/VisualFitsBrowser/App.java
@@ -0,0 +1,13 @@
+package org.wiyn.VisualFitsBrowser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
