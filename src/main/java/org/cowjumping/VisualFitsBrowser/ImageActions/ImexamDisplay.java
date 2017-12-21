0a8b4ecbe691ea3b05e21ea443649ebeca571a5d
Daniel Harbeck <drharbeck@gmail.com>
2017-12-20 17:09:42 -0800

    Text display for image statistics

--- src/main/java/org/cowjumping/VisualFitsBrowser/ImageActions/ImexamDisplay.java
+++ src/main/java/org/cowjumping/VisualFitsBrowser/ImageActions/ImexamDisplay.java
@@ -1,8 +1,10 @@
 package org.cowjumping.VisualFitsBrowser.ImageActions;
 
+import org.apache.log4j.BasicConfigurator;
 import org.cowjumping.FitsUtils.ImageContainer;
 import org.cowjumping.FitsUtils.gaussImage;
 import org.cowjumping.FitsUtils.odiCentroidSupport;
+import org.cowjumping.guiUtils.GUIConsts;
 import org.cowjumping.guiUtils.GuideStarDisplayComponent;
 import org.cowjumping.guiUtils.RadialPlotComponent;
 import org.cowjumping.guiUtils.ZScaleSelectorComponent;
@@ -16,7 +18,7 @@ public class ImexamDisplay extends ImageEvaluator {
 
     private RadialPlotComponent rp;
     private GuideStarDisplayComponent gd;
-
+    private ImageBufferStatusDisplay sd;
     public ImexamDisplay() {
         super();
         setLayout(new BorderLayout());
@@ -24,6 +26,7 @@ public class ImexamDisplay extends ImageEvaluator {
 
         rp = new RadialPlotComponent(250, 250);
         gd = new GuideStarDisplayComponent(250, 250, 0, 65000);
+        sd = new ImageBufferStatusDisplay();
 
         Box h = Box.createVerticalBox();
         {
@@ -40,6 +43,9 @@ public class ImexamDisplay extends ImageEvaluator {
 
         ZScaleSelectorComponent zs = new ZScaleSelectorComponent(gd, 400, 100);
         h.add(zs);
+        h.add (Box.createVerticalGlue());
+        h.add(sd);
+        h.add (Box.createVerticalGlue());
 
         this.add(h, BorderLayout.CENTER);
     }
@@ -68,12 +74,16 @@ public class ImexamDisplay extends ImageEvaluator {
             rp.updateData(gs);
             gd.updateImage(gs.rawImageBuffer, gs.getImageDimX(), gs.getImageDimY(), gs.getCenterX(), gs.getCenterY(), 0);
             gd.setZScale(gs.getBackground() - gs.getBackNoise(), gs.getPeak());
+
+
+            sd.update(gs);
         }
         return -1;
     }
 
 
     public static void main(String args[]) {
+        BasicConfigurator.configure();
         JFrame f = new JFrame();
         ImexamDisplay d = new ImexamDisplay();
         f.getContentPane().add(d);
@@ -88,4 +98,37 @@ public class ImexamDisplay extends ImageEvaluator {
 
 
     }
+}
+
+
+
+class ImageBufferStatusDisplay extends JTextArea {
+
+
+    JTextArea t;
+    public ImageBufferStatusDisplay() {
+        super(40,12);
+
+        setFont(GUIConsts.TTFont16);
+
+    }
+
+
+    public void update (ImageContainer gs) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append ("\n");
+        sb.append(String.format(" center x     % 6.2f [pix]   " , gs.getWindow_Offset_X() + gs.getCenterX() ));
+        sb.append(String.format(" center y     % 6.2f [pix]\n" ,   gs.getWindow_Offset_Y() + gs.getCenterY() ));
+        sb.append(String.format(" FWHM         % 6.2f [pix]\n\n", (gs.getFWHM_X() + gs.getFWHM_Y())/2.));
+        sb.append(String.format(" Flux:      % 10.2f [ADU]\n", gs.getFlux()));
+        sb.append(String.format(" Inst mag       % 5.2f [mag]\n\n", gs.getInstMag()));
+
+        sb.append(String.format(" Sky          % 6.2f \\pm % 4.2f [ADU]\n", gs.getBackground(), gs.getBackNoise()));
+
+        System.out.println (sb);
+        setText(sb.toString());
+
+    }
+
 }
